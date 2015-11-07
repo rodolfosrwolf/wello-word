bcae685a4ac33af8c6cadab8df8412396e5b3151
Rodolfo <rodolfosrwolf@gmail.com>
2015-11-07 09:37:05 -0200

    PHP
    Teste de PHP (Rodolfo)

---
+++ PHP/WebContent/HTML/intro.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Teste PHP</title>
+	</head>
+	<body>
+		<?php 
+			$frutas=array(
+				1=>"Laranja",
+				2=>"Maçã",
+				3=>"Uva");
+			echo"<li>$frutas[1]<br>";
+			echo"<li>$frutas[2]<br>";
+			echo"<li>$frutas[3]<br>";
+			echo"Rodolffos Teste";
+		?>
+	</body>
+</html>
