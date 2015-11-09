bd6c3b7c81be6526ac8874658aad223997b9143c
Rodolfo <rodolfosrwolf@gmail.com>
2015-11-09 10:28:34 -0200

    Teste commit

--- PHP/WebContent/HTML/intro.php
+++ PHP/WebContent/HTML/intro.php
@@ -12,8 +12,9 @@
 			echo"<li>$frutas[2]<br>";
 			echo"<li>$frutas[3]<br>";
 			echo"Rodolffos Teste";
-			echo"Vou faer um outro commit agora"
-			echo"Para mim poder fazer um merge"
+			echo"Vou faer um outro commit agora";
+			echo"Para mim poder fazer um merge";
+			echo"Rodolfo";
 		?>
 	</body>
 </html>
